7e7fe16501ad3344a6f7d48ceaff826ab3c408f6
mrkhan <Nawedk96@gmail.com>
2022-01-19 19:37:59 +0530

    301 related changes

--- src/main/java/in/mrKhan/controller/IMyCotroller.java
+++ src/main/java/in/mrKhan/controller/IMyCotroller.java
@@ -1,5 +1,40 @@
 package in.mrKhan.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.mrKhan.entities.Student;
+import in.mrKhan.service.IService;
+
+@RestController
 public class IMyCotroller {
+	@Autowired
+	IService Service;
+	//creating a get mapping that retrieves all the books detail from the database 
+	@GetMapping("/student")
+	private List<Student> getAllStudents() 
+	{
+	return Service.getAllStudents();
+	}
+	//creating a get mapping that retrieves the detail of a specific book
+	@GetMapping("/student/{studentid}")
+	private Student getBooks(@PathVariable("studentid") int studentid) 
+	{
+	return Service.getStudentsById(studentid);
+	}
+	//creating a delete mapping that deletes a specified book
+	@DeleteMapping("/student/{studentid}")
+	private void deleteBook(@PathVariable("studentid") int studentid) 
+	{
+		Service.delete(studentid);
+	}
 
 }
