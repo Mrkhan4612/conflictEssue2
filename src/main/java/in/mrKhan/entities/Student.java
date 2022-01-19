d00623f958d83768afb4b6446a56cee5d034179e
mrkhan <Nawedk96@gmail.com>
2022-01-19 16:38:47 +0530

    ramesh commit

--- src/main/java/in/mrKhan/entities/Student.java
+++ src/main/java/in/mrKhan/entities/Student.java
@@ -1,5 +1,21 @@
 package in.mrKhan.entities;
 
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Data
+@Entity
 public class Student {
 
+	@Id
+	private int studentId;
+	
+	private String name;
+	
+	private String city;
 }
